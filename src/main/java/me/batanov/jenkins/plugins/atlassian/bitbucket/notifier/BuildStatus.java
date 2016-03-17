3ce1eab084d6715a4ec8c7ec4215eaa3a633fb65
a.jha <abhishek@codenation.co.in>
2016-03-17 20:13:56 +0530

    Matching with remote

--- src/main/java/me/batanov/jenkins/plugins/atlassian/bitbucket/notifier/BuildStatus.java
+++
@@ -1,9 +0,0 @@
-package me.batanov.jenkins.plugins.atlassian.bitbucket.notifier;
-
-/**
- * @author Pavel Batanov <pavel@batanov.me>
- *         2016-01-10 15:54
- */
-public enum BuildStatus {
-    INPROGRESS, SUCCESSFUL, FAILED
-}
--- src/main/java/me/batanov/jenkins/plugins/atlassian/bitbucket/notifier/exception/NotificationFailedException.java
+++
@@ -1,8 +0,0 @@
-package me.batanov.jenkins.plugins.atlassian.bitbucket.notifier.exception;
-
-/**
- * @author Pavel Batanov <pavel@batanov.me>
- *         2016-01-10 16:15
- */
-public class NotificationFailedException extends RuntimeException {
-}
